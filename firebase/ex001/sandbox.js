a5cd71668bf1282b3398e6282f032e451a3781aa
paurigine <78435604+paurigine@users.noreply.github.com>
2021-05-13 16:30:50 +0200

    20210513_1630

--- firebase/ex001/sandbox.js
+++ firebase/ex001/sandbox.js
@@ -7,29 +7,80 @@ const firebaseConfig = {
     messagingSenderId: "1002097449831",
     appId: "1:1002097449831:web:547696516edf76da6aedc2"
 };    
-// Initialize Firebase
+//Initialize Firebase
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
-//---------
 const list = document.querySelector('ul');
-const addRecipe = recipe => {
+const addRecipe = (recipe, id) => {
+    let dg=recipe.created_at.toDate();
+    let day=dg.getDate()+"/"+dg.getMonth()+"/"+dg.getFullYear()+")";
     let html = `
-        <li><div>${recipe.title}</div></li>
-        <li><div>${recipe.created_at.toDate()}</div></li>
-        <li><div>${recipe.autor}</div></li>
+        <li data-id="${id}"><b>${recipe.title}</b> (${day}
+        <button class="btn btn-danger btn-sm my-2">delete</button></li>
     `;
-    console.log(html);
+    // console.log(html);
     list.innerHTML += html;
 };
-db.collection('recipes').get()
-    .then(snapshot => {
-        // console.log(snapshot.docs[0].data());
-        snapshot.forEach(doc => {
-            // console.log(doc.data());
-            addRecipe(doc.data());
-        });
-    })
-    .catch(err => console.log(err));
-function seguent(){
-    a++;
-}
+// delete documents
+list.addEventListener('click', e => {
+    // console.log(e);
+    if(e.target.tagName === 'BUTTON') {
+        const id = e.target.parentElement.getAttribute('data-id');
+         //console.log(id);
+        db.collection('recipes').doc(id).delete()
+            //.then(() => console.log('recipe deleted!'))
+            .catch((err) => console.log(err));
+    }
+});
+// get documents
+// db.collection('recipes').get()
+//     .then(snapshot => {
+//         // console.log(snapshot.docs[0].data());
+//         snapshot.forEach(doc => {
+//             // console.log(doc.data());
+//             // console.log(doc.id);
+//             addRecipe(doc.data(),doc.id);
+//         });
+//     })
+//     .catch(err => console.log(err));
+// add documents
+const form = document.querySelector('form');
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    let now = new Date();
+    const recipe = {
+        title: form.recipe.value,
+        created_at: firebase.firestore.Timestamp.fromDate(now)
+    };
+    db.collection('recipes').add(recipe)
+    //.then(() => console.log('recipe added!'))
+    .catch(err => console.log(err))
+});
+const deleteRecipe = id => {
+    const recipes = document.querySelectorAll('li');
+    recipes.forEach(recipe => {
+        if(recipe.getAttribute('data-id') === id) {
+            recipe.remove();
+        }
+    });
+};
+// real time listener
+const unsubscribe = db.collection('recipes').onSnapshot(snapshot => {
+    // console.log(snapshot.docChanges());
+    snapshot.docChanges().forEach(change => {
+        // console.log(change);
+        const doc = change.doc;
+        // console.log(doc);
+        if (change.type === 'added') {
+            addRecipe(doc.data(), doc.id);
+        } else if (change.type === 'removed') {
+            deleteRecipe(doc.id);
+        }
+    });
+});
+const unsubscribeBtn = document.querySelector('.unsubscribe');
+// unsubscribe from database changes
+unsubscribeBtn.addEventListener('click', () => {
+    unsubscribe();
+    console.log('you unsubscribed from all changes');
+});
