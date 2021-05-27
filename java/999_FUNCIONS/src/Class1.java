5414a74a4b258b2a7b0ada560a5315a99fdb28b8
paurigine <pauriera@ginebro.cat>
2021-05-27 19:19:50 +0200

    20210527_1920

---
+++ java/999_FUNCIONS/src/Class1.java
@@ -0,0 +1,8 @@
+
+public class Class1 {
+
+	Class1(){
+		super();//crea un objecte pare 
+	}
+	
+}
--- java/999_FUNCIONS/src/Llibreria.java
+++ java/999_FUNCIONS/src/Llibreria.java
@@ -1,36 +1,54 @@
 import java.io.File;
 import java.io.FileWriter;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.InputMismatchException;
+import java.util.Iterator;
 import java.util.Scanner;
 
 public class Llibreria {
 	public static void main(String[] args) {
+		//------------------------------------------- N->int S->String L->List C->char A->Array M->Matriu
+		//----------mostrar
 		System.out.print("");
 		System.out.println("");
-		Scanner TInt = new Scanner(System.in);
-		Scanner TStr = new Scanner(System.in);
-		int N=0;
-		String S="";
-		String A[]=new String[2];
-		S = TStr.nextLine();
-		S = TStr.next();
-		N = TInt.nextInt();
-		N	 = Integer.parseInt(TStr.nextLine());
-		//return Arrays.asList(A).contains(S); //torna true false
+		//----------declaraccio
+		int N=0;//int
+		String S="";//string
+		String A[]=new String[2];//array
+		String M[][]=new String[2][2];//matriu
+		Class1[] arrObj = new Class1[N];//declaracio de array d'objectes
+		arrObj[N] = new Class1();//creaccio d'objecte a l'array
+		Scanner TStr = new Scanner(System.in);//scanner
+		ArrayList L = new ArrayList();//list
+		//-----------demanar
+		S = TStr.nextLine();//demanar string
+		N = Integer.parseInt(TStr.nextLine());//demanar int
+		char C = TStr.nextLine().charAt(0);//demanar char
+		//-----------comversions
 		N=Integer.parseInt(S);//string to int
 		S = Character.toString(N);//ascii to string
 		Character.getNumericValue(N);//ascii to int
+		Math.sqrt(2);//arrel quadrada
+		Math.pow(1,2);// 1 elevat a 2
+		S.toLowerCase();//string a minuscules
+		S.toUpperCase();//string a majuscules
+		L.add("1");//contingut  | afegir
+		L.get(0);//posicio | agafar
+		L.add(0,2);//posicio/contingut | modificar
+		L.size();//length | longitud
+		L.remove(0);//posicio | treure
+		//-----------switch
 		switch(N) {
 		case 1:
 			break;
 		case 2:
 			break;
 		}
+		//-----------exepcions
 		try {
-			//this.S="";
 		}catch(InputMismatchException e) {
-   		 System.out.println("Error! ");
+   		 	System.out.println("Error! ");
    	 	}catch(ArithmeticException e){
 			System.out.println("ERROR! X/0");
 		}catch (NumberFormatException e) {
@@ -41,8 +59,8 @@ public class Llibreria {
 			System.out.println("ERROR! general");
 		}
 	}
-	public static Scanner TInt = new Scanner(System.in);
 	public static Scanner TStr = new Scanner(System.in);
+	public static int O = 0;
 	public static void llegir(String[] matriu) {//llegir array
 	for(int i=0;i!=matriu.length;i++) {
 			System.out.print(matriu[i]+" ");
@@ -163,11 +181,45 @@ public class Llibreria {
 	public static String mat() {//generar matricula aleatoria
 		return ""+RNG(0,9)+RNG(0,9)+RNG(0,9)+RNG(0,9)+Character.toString(RNG(65,90))+Character.toString(RNG(65,90))+Character.toString(RNG(65,90));
 	}
-	
-	
-	
-	
-	
+	public String toString() {//mostrar objecte
+		return "";
+	}
+	public static boolean Has(String[] A,String S) {//torna true false si conte s
+		return Arrays.asList(A).contains(S); 
+	}
+	public static void aObj() {//omplir array de objectes
+		Class1[] arrObj = new Class1[O];//declaracio de array d'objectes
+		for (int i = 0; i < arrObj.length; i++) {
+			System.out.println("Dades per a Objecte");
+			System.out.print("Introdueix el llibre Objecte : ");
+			String llibre = TStr.nextLine();
+			System.out.print("Introdueix el autor Objecte : ");
+			String autor = TStr.nextLine();
+			System.out.print("Introdueix la quantitat d'exemplars Objecte : ");
+			String quant = TStr.nextLine();
+			String prestec="0";
+			arrObj[i] = new Class1();//creaccio d'objecte en l'array
+		}
+	}
+	public class class2 extends Class1{	
+	}
+	public static void iterator(ArrayList L) {//iterator
+		Iterator i = L.iterator();
+		while(i.hasNext()) {
+			System.out.println(i.next());
+		}
+	}
+	public static void contarproductes(ArrayList L) {
+		Iterator i = L.iterator();
+		int unitats=0;
+		while(i.hasNext()) {
+			Object contingut = i.next();
+			if(contingut instanceof Class) {
+				Class tmp  = (Class)contingut;
+				//unitats+=tmp.getQuantitat();
+			}
+		}
+	}
 	
 	
 	
